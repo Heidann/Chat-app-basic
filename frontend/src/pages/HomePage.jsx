5dd92b6076e5d16ffbcad820a0e94f0c0e6fe278
HeiDann <haidang203033@gmail.com>
2025-03-13 15:54:42 +0700

    fix: fix display HomePage (#8)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,8 +7,8 @@ import NoChatSelected from "../components/NoChatSelected";
 const HomePage = () => {
   const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200">
-      <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh - 8rem)]">
+    <div className="h-screen bg-base-200 pt-16 ">
+      <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-full h-full p-3">
         <div className="flex h-full rounded-lg overflow-hidden">
           <Sidebar />
 
